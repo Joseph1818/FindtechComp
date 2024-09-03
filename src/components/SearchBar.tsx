8f02fa81fbdab19a6200cfa943f6e60f15c84839
Umba joseph <Josephdjamba04@gmail.com>
2024-09-03 22:16:12 +0200

    add/deploy error fix 2

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 
@@ -7,55 +7,58 @@ interface DataItem {
   suburb: string;
   email: string;
   phoneNum: string;
-  physicalAdress: string;
-  link: string,
+  physicalAddress: string;
+  link: string;
 }
 
 interface SearchBarProps {
   placeholder: string;
-  data: DataItem[]; 
+  data: DataItem[];
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({ placeholder, data }) => {
-
-  const [filterData, setFilterData] = useState <DataItem[]>([]);
+  const [filterData, setFilterData] = useState<DataItem[]>([]);
 
   const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchFilter = event.target.value;
 
     const newFilter = data.filter((value) => {
       return value.name.toLowerCase().includes(searchFilter.toLowerCase());
-    })
+    });
 
-    if (searchFilter === ""){
+    if (searchFilter === "") {
       setFilterData([]);
     } else {
-    setFilterData(newFilter);
+      setFilterData(newFilter);
     }
-  }
+  };
 
   const clearInput = () => {
-  setFilterData([]);
-  
-  }
+    setFilterData([]);
+  };
 
   return (
-    <div className='input'>
-      <div className='searchInputs'>
-        <input type='text' placeholder={placeholder} onChange={handleFilter} />
-        <div className='searchIcon'>
-          {filterData.length === 0 ? <SearchIcon /> : <CloseIcon id="clearButton" onClick={clearInput} /> }
+    <div className="input">
+      <div className="searchInputs">
+        <input type="text" placeholder={placeholder} onChange={handleFilter} />
+        <div className="searchIcon">
+          {filterData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearButton" onClick={clearInput} />
+          )}
         </div>
       </div>
 
-      { filterData.length !== 0 && ( <div className='dataResult'>
-        {filterData.map((value, key) => (
-          <div className="" key={key}>
-            <p className='dataItem'>{value.name}</p>
-          </div>
-        ))}
-      </div>)}
-    
+      {filterData.length !== 0 && (
+        <div className="dataResult">
+          {filterData.map((value, key) => (
+            <div className="" key={key}>
+              <p className="dataItem">{value.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
